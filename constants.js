d6ac330be6742ee742e588e1bbd42c8357f573c5
pablocordonie <pablogarcia.93.06@gmail.com>
2023-01-17 01:06:00 +0100

    feat: finish a navbar component UI

---
+++ constants.js
@@ -0,0 +1,22 @@
+export const DOCUMENTATION = [
+    {
+        title: 'Lo mejor de React',
+        url: '',
+        favorite: true,
+    },
+    {
+        title: 'Documentación de React',
+        url: '',
+        favorite: false,
+    },
+    {
+        title: 'Documentación de JS',
+        url: '',
+        favorite: true,
+    },
+    {
+        title: 'Ejemplos de CSS',
+        url: '',
+        favorite: false,
+    },
+];
